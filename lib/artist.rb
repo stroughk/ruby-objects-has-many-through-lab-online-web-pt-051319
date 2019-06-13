35dee6d4aed359ffc9576b562766d9630624cb65
Kateryna Strough <katerynastrough@hotmail.com>
2019-06-13 22:12:52 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,4 +17,12 @@ class Artist
     Song.new(name, self, genre)
   end 
   
+  def songs 
+    Song.all.select { |song| song.artist == self }
+  end
+  
+  def genres 
+    songs.map(&:genre)
+  end 
+  
 end
